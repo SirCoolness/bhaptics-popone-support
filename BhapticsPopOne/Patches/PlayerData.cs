a853aa5848b459c20b2f9cdb276740e917f4b9ac
Joon <sam960313@gmail.com>
2020-12-16 15:38:53 +0900

    MelonLoader Logging with SyncHealth

---
+++ BhapticsPopOne/Patches/PlayerData.cs
@@ -0,0 +1,19 @@
+﻿using BhapticsPopOne.Haptics;
+using Harmony;
+using MelonLoader;
+
+namespace BhapticsPopOne.Patches.PlayerData2
+{
+    [HarmonyPatch(typeof(PlayerData), "SyncHealth")]
+    public class HandlePlayerHit
+    {
+        static void Prefix(PlayerData __instance, int oldValue, int newValue)
+        {
+            if (__instance != Mod.Instance.Data.Players.LocalPlayerContainer.playerData)
+                return;
+
+            MelonLogger.Log(System.ConsoleColor.Blue, "Name: " + __instance.DisplayName);
+            MelonLogger.Log(System.ConsoleColor.Red, "Health: " + newValue);
+        }
+    }
+}
