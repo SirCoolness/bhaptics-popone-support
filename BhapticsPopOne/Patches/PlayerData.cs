ffea61521c55cf78bea4096b4395e2328c45e232
Joon <sam960313@gmail.com>
2020-12-16 19:16:39 +0900

    Heartbeat when Announcer tells Low Health (under 25)

--- BhapticsPopOne/Patches/PlayerData.cs
+++ BhapticsPopOne/Patches/PlayerData.cs
@@ -7,14 +7,13 @@ namespace BhapticsPopOne.Patches.PlayerData2
     [HarmonyPatch(typeof(PlayerData), "SyncHealth")]
     public class HandlePlayerHit
     {
+        // heartbeat when HP under 25
         static void Prefix(PlayerData __instance, int oldValue, int newValue)
         {
             if (__instance != Mod.Instance.Data.Players.LocalPlayerContainer.playerData)
                 return;
 
-            MelonLogger.Log(System.ConsoleColor.Red, "Health: " + newValue);
-
-            if(newValue < 20)
+            if(newValue < 25 && oldValue >= 25)
             {
                 PatternManager.LowHealthHeartbeat();
             }
