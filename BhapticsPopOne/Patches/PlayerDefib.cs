4836e2e177e8d40cd06ad110fa834cfbb1d189bf
Joon <sam960313@gmail.com>
2020-12-18 19:56:13 +0900

    Haptics for Rubbing

---
+++ BhapticsPopOne/Patches/PlayerDefib.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BigBoxVR;
+using Harmony;
+using MelonLoader;
+using BhapticsPopOne.Haptics;
+using BigBox.PopOne.Unity;
+
+namespace BhapticsPopOne.Patches
+{
+    [HarmonyPatch(typeof(PlayerDefib), "State", MethodType.Setter)]
+    public class OnDefibStateChanged
+    {
+        // defibrillator charging with haptics
+        static void Prefix(HandControllerMediator __instance, PlayerDefib.DefibState value)
+        {
+            MelonLogger.Log(ConsoleColor.Blue, "Defib : " + value);
+
+            if (value == PlayerDefib.DefibState.Rubbing)
+            {
+                PatternManager.RubbingDefib();
+            }
+
+            if (value == PlayerDefib.DefibState.Charged)
+            {
+                PatternManager.ChargedDefib();
+            }
+        }
+    }
+}
