b1e4eb29e10c0dd22a6cc91615f556d160e9f85f
Akiva Silver <akiva.silver@gmail.com>
2020-12-21 00:22:27 -0500

    created effects for swapping items

---
+++ BhapticsPopOne/Patches/PlayerInventory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Harmony;
+using MelonLoader;
+
+namespace BhapticsPopOne.PlayerInventory2
+{
+    [HarmonyPatch(typeof(PlayerInventory), "NetworkequipIndex", MethodType.Setter)]
+    public class NetworkequipIndexSetter
+    {
+        static void Prefix(PlayerInventory __instance, int value)
+        {
+            MelonLogger.Log(ConsoleColor.Gray, value);
+            if (!__instance.isLocalPlayer)
+                return;
+        }
+    }
+}
