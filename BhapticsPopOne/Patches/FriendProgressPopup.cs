2254a503d30a0724e7a942be8fcff4a14558cf18
Akiva Silver <akiva.silver@gmail.com>
2020-12-25 15:31:14 -0500

    added fist bump

---
+++ BhapticsPopOne/Patches/FriendProgressPopup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BhapticsPopOne.Haptics.Patterns;
+using Harmony;
+using MelonLoader;
+
+namespace BhapticsPopOne.FriendProgressPopup2
+{
+    [HarmonyPatch(typeof(FriendProgressPopup), "Init")]
+    public class Init
+    {
+        static void Prefix(FriendProgressPopup __instance, float length, string name)
+        {
+            FistBump.Start(length);
+        }
+    }
+    
+    [HarmonyPatch(typeof(FriendProgressPopup), "Finish")]
+    public class Finish
+    {
+        static void Prefix(FriendProgressPopup __instance, bool wasCompleted)
+        {
+            FistBump.Stop(wasCompleted);
+        }
+    }
+}
