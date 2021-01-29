1d5732904a7ebe235b9581992a8c36cc6caec217
Akiva Silver <me@sircoolness.dev>
2021-01-28 23:00:01 -0500

    bumped version

--- BhapticsPopOne/Properties/AssemblyInfo.cs
+++ BhapticsPopOne/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using MelonLoader;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: MelonInfo(typeof(BhapticsPopOne.Mod), "Bhaptics Support", "0.0.18", "SirCoolness, jOoN, Sangwon")]
+[assembly: MelonInfo(typeof(BhapticsPopOne.Mod), "Bhaptics Support", "0.0.19", "SirCoolness, jOoN, Sangwon")]
 [assembly: MelonGame("BigBoxVR", "POPULATION: ONE")]
