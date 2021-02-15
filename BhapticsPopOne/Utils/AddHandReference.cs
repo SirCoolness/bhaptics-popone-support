28330d8e5b9b4ffaa50f473bb3458dd90f0ed2d3
Akiva Silver <me@sircoolness.dev>
2021-02-15 04:03:54 -0500

    added extra RB for extra triggering

--- BhapticsPopOne/Utils/AddHandReference.cs
+++ BhapticsPopOne/Utils/AddHandReference.cs
@@ -1,4 +1,5 @@
-﻿using BhapticsPopOne.MonoBehaviours;
+﻿using BhapticsPopOne.Haptics.Patterns;
+using BhapticsPopOne.MonoBehaviours;
 using UnityEngine;
 
 namespace BhapticsPopOne
@@ -9,16 +10,72 @@ namespace BhapticsPopOne
 
         public static void AddHandsToPlayer(PlayerContainer player)
         {
-            ApplyComponents(player.netId, Handedness.Left, AddHand(player.Avatar?.HandLeftAttachPoint?.gameObject, new []
-            {
-                player.Avatar.HandLeft.gameObject.GetComponent<CapsuleCollider>()
-            }));
-            ApplyComponents(player.netId, Handedness.Right, AddHand(player.Avatar?.HandRightAttachPoint?.gameObject, new []
-            {
-                player.Avatar.HandRight.gameObject.GetComponent<CapsuleCollider>()
-            }));
+            // is this the way to do things, no.
+            Collider[] leftIgnore;
+            Collider[] rightIgnore;
+
+            var leftRBRes = AddColliderHand(player.Avatar?.HandLeftAttachPoint?.gameObject, Handedness.Left);
+            var rightRBRes = AddColliderHand(player.Avatar?.HandRightAttachPoint?.gameObject, Handedness.Right);
+
+            if (leftRBRes == null)
+                leftIgnore = new Collider[]
+                {
+                    player.Avatar.HandLeft.gameObject.GetComponent<CapsuleCollider>()
+                };
+            else
+                leftIgnore = new Collider[]
+                {
+                    player.Avatar.HandLeft.gameObject.GetComponent<CapsuleCollider>(),
+                    leftRBRes.GetComponent<SphereCollider>()
+                };
+            
+            if (rightRBRes == null)
+                rightIgnore = new Collider[]
+                {
+                    player.Avatar.HandRight.gameObject.GetComponent<CapsuleCollider>()
+                };
+            else
+                rightIgnore = new Collider[]
+                {
+                    player.Avatar.HandRight.gameObject.GetComponent<CapsuleCollider>(),
+                    rightRBRes.GetComponent<SphereCollider>()
+                };
+            
+            ApplyComponents(player.netId, Handedness.Left, AddHand(player.Avatar?.HandLeftAttachPoint?.gameObject, leftIgnore));
+            ApplyComponents(player.netId, Handedness.Right, AddHand(player.Avatar?.HandRightAttachPoint?.gameObject, rightIgnore));
         }
 
+        private static GameObject AddColliderHand(GameObject dest, Handedness hand)
+        {
+            string name = $"{nameof(AddHandReference)}{HapticUtils.HandExt(hand)}";
+            
+            if (dest?.transform.Find(name) != null)
+                return null;
+            
+            var gameObject = new GameObject(name);
+            gameObject.transform.parent = dest.transform;
+            gameObject.transform.localPosition = Vector3.zero;
+            gameObject.layer = 19;
+            gameObject.transform.localScale = new Vector3(width, width, width);
+            
+            var rb = gameObject.AddComponent<Rigidbody>();
+            rb.useGravity = false;
+            rb.isKinematic = true;
+            rb.constraints = RigidbodyConstraints.FreezeRotation;
+            rb.collisionDetectionMode = CollisionDetectionMode.Discrete;
+            
+            var collider = gameObject.AddComponent<SphereCollider>();
+            collider.radius = width / 2f;
+            collider.isTrigger = false;
+            
+            var proxy = gameObject.AddComponent<CustomPhysicsObjectProxy>();
+            proxy.useBounce = false;
+            proxy.PhysicsObjectType = PhysicsObjectType.Dynamic;
+            proxy.ColliderUpdatePolicy = ColliderUpdatePolicy.All;
+
+            return gameObject;
+        }
+        
         private static GameObject AddHand(GameObject dest, Collider[] ignoreC)
         {
             if (dest?.transform.Find(nameof(AddHandReference)) != null)
