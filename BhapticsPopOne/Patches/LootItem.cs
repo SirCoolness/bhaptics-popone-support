a1ed33927df1f6322cfa8a2a7813f0441657b93d
Akiva Silver <me@sircoolness.dev>
2021-02-26 02:53:20 -0500

    fixed looting

--- BhapticsPopOne/Patches/LootItem.cs
+++ BhapticsPopOne/Patches/LootItem.cs
@@ -9,42 +9,42 @@ using UnityEngine;
 
 namespace BhapticsPopOne.LootItem2
 {
-    // [HarmonyPatch(typeof(LootItem), "FlyItem")]
-    // public class FlyItem
-    // {
-    //     static void Prefix(LootItem __instance, PlayerContainer container, bool dominantHand)
-    //     {
-    //         if (!container.isLocalPlayer)
-    //             return;
-    //
-    //         LocalProperties properties = GoyfsHelper.Get<LocalProperties>();
-    //         
-    //         // TODO: make better
-    //         Handedness hand;
-    //
-    //         if (properties.DominantHand == properties.LeftHand)
-    //         {
-    //             hand = dominantHand ? Handedness.Left : Handedness.Right;
-    //         } else if (properties.DominantHand == properties.RightHand)
-    //         {
-    //             hand = dominantHand ? Handedness.Right : Handedness.Left;
-    //         }
-    //         else 
-    //             return;
-    //
-    //         PickupItem.Execute(hand);
-    //     }
-    // }
+    [HarmonyPatch(typeof(LootItem), "FlyItem")]
+    public class FlyItem
+    {
+        static void Prefix(LootItem __instance, PlayerContainer container, bool dominantHand)
+        {
+            if (!container.isLocalPlayer)
+                return;
     
-    // [HarmonyPatch(typeof(LootItem), "EjectWithVelocity")]
-    // public class EjectWithVelocity
-    // {
-    //     static void Prefix(LootItem __instance, Vector3 ejectVelocity)
-    //     {
-    //         if (!__instance.netIdentity.isLocalPlayer)
-    //             return;
-    //         
-    //         DropItem.Execute();
-    //     }
-    // }
+            LocalProperties properties = GoyfsHelper.Get<LocalProperties>();
+            
+            // TODO: make better
+            Handedness hand;
+    
+            if (properties.DominantHand == properties.LeftHand)
+            {
+                hand = dominantHand ? Handedness.Left : Handedness.Right;
+            } else if (properties.DominantHand == properties.RightHand)
+            {
+                hand = dominantHand ? Handedness.Right : Handedness.Left;
+            }
+            else 
+                return;
+    
+            PickupItem.Execute(hand);
+        }
+    }
+    
+    [HarmonyPatch(typeof(LootItem), "EjectWithVelocity")]
+    public class EjectWithVelocity
+    {
+        static void Prefix(LootItem __instance, Vector3 ejectVelocity)
+        {
+            if (!__instance.netIdentity.isLocalPlayer)
+                return;
+            
+            DropItem.Execute();
+        }
+    }
 }
