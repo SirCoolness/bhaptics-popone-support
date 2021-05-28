4e10950c70d576947b40786f857775fe70062575
Akiva Silver <akiva.silver@pm.me>
2021-05-28 00:54:03 -0400

    Fixed rendering issues for liv

---
+++ LivFix/Patch.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using Harmony;
+using MelonLoader;
+using BhapticsPopOne.Utils;
+using UnityEngine;
+
+namespace LivFix
+{
+    public class LivFix : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            Patreon.Run(); // (●'◡'●)
+        }
+    }
+
+    [HarmonyPatch(typeof(LIV.SDK.Unity.LIV), "OnSDKActivate")]
+    public class OnSDKActivate
+    {
+        static void Postfix(LIV.SDK.Unity.LIV __instance)
+        {
+            __instance._fixPostEffectsAlpha = false;
+            __instance._spectatorLayerMask = (__instance._spectatorLayerMask.value & ~(1 << 7)) | (0 << 7);
+            __instance._spectatorLayerMask = (__instance._spectatorLayerMask.value & ~(1 << 5)) | (1 << 5);
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerAvatar), "UpdateSkinRenderStates")]
+    public class UpdateSkinRenderStates
+    {
+        static void Postfix(PlayerAvatar __instance)
+        {
+            GameAwake.OnPlayerAdded(__instance.Container);
+        }
+    }
+
+    [HarmonyPatch(typeof(BattleContextView), "Awake")]
+    public class GameAwake
+    {
+        static void Postfix()
+        {
+            if (!BindGoyfs())
+                MelonLogger.Error("Failed to bind to Goyfs");
+        }
+        
+        public static bool BindGoyfs()
+        {
+            bool[] bindStatuses = new bool[]
+            {
+            };
+
+            List<int> failures = new List<int>();
+            for (var i = 0; i < bindStatuses.Length; i++)
+            {
+                if (bindStatuses[i])
+                    continue;
+                
+                failures.Add(i);
+            }
+
+            if (failures.Count > 0)
+            {
+                MelonLogger.Error($"Failed to binding to Goyfs: [{String.Join(", ", failures)}]");
+            }
+
+            return failures.Count == 0;
+        }
+        public static void OnPlayerAdded(PlayerContainer container)
+        {
+            if (container.transform.root != container.transform || container.Avatar?.Rig == null || !container.Data.IsReady)
+                return;
+
+            LocalProperties properties = GoyfsHelper.Get<LocalProperties>();
+            if (properties.Container.netId != container.netId)
+                return;
+            
+            if (properties.Container.Avatar?.SkinObj == null)
+                return;
+
+            var renderer = properties.Container.Avatar.SkinObj.GetComponentInChildren<MeshRenderer>();
+            renderer.gameObject.layer = 7;
+        }
+        
+    }
+}
---
+++ LivFix/Patreon.cs
@@ -0,0 +1,163 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MelonLoader;
+using UnityEngine;
+
+namespace LivFix
+{
+    /**
+     * This class is a thank you to all the Patreon members
+     */
+    public class Patreon
+    {
+        public static ConsoleColor color = ConsoleColor.Yellow;
+        public static string[] ExistContributors = new string[]
+        {
+            "Jennifer Lee @ bHaptics"
+        };
+
+        /**
+         * People exist. Some people are just extremely generous ♥.
+         *
+         * Exist:
+         * - Jennifer Lee @ bHaptics
+         */
+        public static void ThankExist()
+        {
+            if (ExistContributors.Length <= 0)
+                return;
+            
+            int rowSize = 1;
+            int offsetLen = Mathf.CeilToInt(ExistContributors.Length / (float) rowSize);
+
+            MelonLogger.Msg(color, "Thank you to the Exist contributors:");
+            for (int i = 0; i < offsetLen; i++)
+            {
+                MelonLogger.Msg(color, PatreonHelper(ExistContributors, i, rowSize));
+            }
+        }
+        
+        public static string[] BurgerKingContributors = new string[]
+        {
+            "AceJas",
+            "ZstormGames",
+            "NerdNational",
+            "Anonymous Supporter",
+            "TheMysticle",
+            "Rasta",
+            "bsharp",
+            "Sky Candy"
+        };
+
+        /**
+         * Thank you to the Burger King tier contributors.
+         * Much Burger King will be consumed in your name.
+         *
+         * Burger King:
+         * - AceJas
+         * - ZstormGames
+         * - NerdNational
+         * - Anonymous Supporter [JR]
+         * - TheMysticle
+         * - Rasta
+         * - bsharp
+         * - Sky Candy
+         */
+        public static void ThankBurgerKing()
+        {
+            if (BurgerKingContributors.Length <= 0)
+                return;
+
+            int rowSize = 3;
+            int offsetLen = Mathf.CeilToInt(BurgerKingContributors.Length / (float) rowSize);
+
+            MelonLogger.Msg(color, "Thank you to the Burger King contributors:");
+            for (int i = 0; i < offsetLen; i++)
+            {
+                MelonLogger.Msg(color, PatreonHelper(BurgerKingContributors, i, rowSize));
+            }
+        }
+        
+        public static string[] CoffeeContributors = new string[]
+        {
+            "Alex VR",
+            "Ham VR",
+            "Delistd",
+            "Mike.",
+            "Maker124",
+            "Pally's Crew",
+            "AprilPvd"
+        };
+
+        /**
+         * I don't like coffee but I love the people who give me coffee.
+         * I may try to get over the fear of the beans some day.
+         *
+         * Coffee:
+         * - Alex VR
+         * - Ham VR
+         * - Delistd
+         * - Mike.
+         * - Maker124
+         * - Pally's Crew
+         * - AprilPvd
+         */
+        public static void ThankCoffee()
+        {
+            if (CoffeeContributors.Length <= 0)
+                return;
+            
+            int rowSize = 5;
+            int offsetLen = Mathf.CeilToInt(CoffeeContributors.Length / (float) rowSize);
+
+            MelonLogger.Msg(color, "Thank you to the Coffee contributors:");
+            for (int i = 0; i < offsetLen; i++)
+            {
+                MelonLogger.Msg(color, PatreonHelper(CoffeeContributors, i, rowSize));
+            }
+        }
+        
+        /**
+         * (☞ﾟヮﾟ)☞☜(ﾟヮﾟ☜)
+         */
+        public static void Promote()
+        {
+            MelonLogger.Msg(color, "If you would like to support future/ongoing projects and get your name added here.");
+            MelonLogger.Msg(color, "Check out: https://www.patreon.com/SirCoolness");
+        }
+
+        /**
+         * Thank you
+         */
+        public static void Run()
+        {
+            ThankExist();
+            if (ExistContributors.Length > 0)
+                MelonLogger.Msg(color, "");
+            ThankBurgerKing();
+            if (BurgerKingContributors.Length > 0)
+                MelonLogger.Msg(color, "");
+            ThankCoffee();
+            if (CoffeeContributors.Length > 0)
+                MelonLogger.Msg(color, "");
+            Promote();
+        }
+
+        private static string PatreonHelper(string[] patreons, int index, int rowSize)
+        {
+            List<string> names = new List<string>();
+            
+            for (int i = 0; i < rowSize; i++)
+            {
+                var offset = (index * rowSize) + i;
+                if (patreons.Length <= offset)
+                    break;
+                
+                names.Add(patreons[offset]);
+            }
+
+            return String.Join(", ", names);
+        }
+    }
+}
