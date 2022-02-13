a9811bd351723d4a2860cda27d2dfa5f686e91ae
Akiva Silver <akiva.silver@pm.me>
2022-02-12 23:37:08 -0500

    hotfix: disable buff haptics

--- BhapticsPopOne/Patches/PlayerBuffUsableBehaviour.cs
+++ BhapticsPopOne/Patches/PlayerBuffUsableBehaviour.cs
@@ -1,36 +1,36 @@
-﻿using System;
-using System.Collections;
-using System.Runtime.InteropServices;
-using BhapticsPopOne.Haptics;
-using BhapticsPopOne.Haptics.Patterns;
-using BigBoxVR;
-using BigBoxVR.BattleRoyale.Models.Shared;
-using HarmonyLib;
-using MelonLoader;
-using UnhollowerBaseLib;
-
-namespace BhapticsPopOne
-{
-    [HarmonyPatch(typeof(PlayerBuffUsableBehaviour), "State", MethodType.Setter)]
-    public class OnBuffStateChanged
-    {
-        static void Prefix(PlayerBuffUsableBehaviour __instance, BuffState value)
-        {
-            if (!(__instance?.PlayerUsable?.isLocalPlayer == true))
-            {
-                return;
-            }
-
-            if (__instance.PlayerUsable.EquippedSlot.ItemType == InventoryItemType.BuffEnergyDrink || __instance.PlayerUsable.EquippedSlot.ItemType == InventoryItemType.BuffShieldDrink)
-            {
-                DrinkSoda.Execute(__instance.PlayerUsable.EquippedSlot.ItemType, __instance.Info.TimeToApply, value);
-            } else if (__instance.PlayerUsable.EquippedSlot.ItemType == InventoryItemType.BuffBanana)
-            {
-                Food.EatBanana(value);
-            } else if (__instance.PlayerUsable.EquippedSlot.ItemType == InventoryItemType.BuffShieldShaker)
-            {
-                Shaker.Consume(value);
-            }
-        }
-    }
-}
+﻿// using System;
+// using System.Collections;
+// using System.Runtime.InteropServices;
+// using BhapticsPopOne.Haptics;
+// using BhapticsPopOne.Haptics.Patterns;
+// using BigBoxVR;
+// using BigBoxVR.BattleRoyale.Models.Shared;
+// using HarmonyLib;
+// using MelonLoader;
+// using UnhollowerBaseLib;
+//
+// namespace BhapticsPopOne
+// {
+//     [HarmonyPatch(typeof(PlayerBuffUsableBehaviour), "State", MethodType.Setter)]
+//     public class OnBuffStateChanged
+//     {
+//         static void Prefix(PlayerBuffUsableBehaviour __instance, BuffState value)
+//         {
+//             if (!(__instance?.PlayerUsable?.isLocalPlayer == true))
+//             {
+//                 return;
+//             }
+//
+//             if (__instance.PlayerUsable.EquippedSlot.ItemType == InventoryItemType.BuffEnergyDrink || __instance.PlayerUsable.EquippedSlot.ItemType == InventoryItemType.BuffShieldDrink)
+//             {
+//                 DrinkSoda.Execute(__instance.PlayerUsable.EquippedSlot.ItemType, __instance.Info.TimeToApply, value);
+//             } else if (__instance.PlayerUsable.EquippedSlot.ItemType == InventoryItemType.BuffBanana)
+//             {
+//                 Food.EatBanana(value);
+//             } else if (__instance.PlayerUsable.EquippedSlot.ItemType == InventoryItemType.BuffShieldShaker)
+//             {
+//                 Shaker.Consume(value);
+//             }
+//         }
+//     }
+// }
