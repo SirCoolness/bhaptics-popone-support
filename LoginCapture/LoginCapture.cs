eeac5f5e23d1a5f4cdf88c5486098d3d536d187c
Akiva Silver <akiva.silver@pm.me>
2022-05-11 19:36:31 -0400

    fixed login capturing

--- LoginCapture/LoginCapture.cs
+++ LoginCapture/LoginCapture.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.IO;
+using System.Net.Mime;
+using System.Threading;
 using BigBoxVR;
-using HarmonyLib;
 using MelonLoader;
+using PlayFab;
 using PlayFab.ClientModels;
 using UnityEngine;
+using Harmony;
+using Object = UnityEngine.Object;
 
-[assembly: MelonInfo(typeof(LoginCapture.LoginCapture), "Login Capture", "0.0.1", "SirCoolness")]
+[assembly: MelonInfo(typeof(LoginCapture.LoginCapture), "Login Capture", "0.0.2", "SirCoolness")]
 [assembly: MelonGame("BigBoxVR", "Population: ONE")]
 
 namespace LoginCapture
@@ -15,27 +19,57 @@ namespace LoginCapture
     {
         public override void OnLevelWasLoaded(int level)
         {
-            if (level == 0)
-            {
-                GameObject.Find("/SplashManager").GetComponent<SplashManager>().OnAnimationFinished();
-            }
+            if (level != 0)
+                return;
+            
+            StartAuthentication();
+            
+            // InstallFiles.IsGameInstalled();
+            
+            // Application.Quit();
         }
 
         public static void Finalize(LoginResult result)
         {
+            MelonLogger.Msg("Complete");
+            
             var outputDir = Path.Combine(MelonHandler.ModsDirectory, nameof(LoginCapture));
             var outputFile = Path.Combine(outputDir, "login.json");
             
             Directory.CreateDirectory(outputDir);
             File.WriteAllText(outputFile, result.ToJson());
 
+            // InstallFiles.IsGameInstalled();
+
             MelonLogger.Msg($"Saved session to [{outputFile}]");
             
             Application.Quit(0);
+            Thread.Sleep(100000000);
+        }
+
+        public static void StartAuthentication()
+        {
+            var playfabConnection = Object.FindObjectOfType<PlayFabConnection>();
+
+            if (playfabConnection != null)
+            {
+                MelonLogger.Msg("connection found");
+                return;
+            }
+            
+            MelonLogger.Msg("creating connection");
+            
+            var obj = new GameObject();
+            obj.name = "AuthHelper";
+
+            playfabConnection = obj.AddComponent<PlayFabConnection>();
+
+            Object.DontDestroyOnLoad(playfabConnection);
+            MelonLogger.Msg("added");
         }
     }
     
-    [HarmonyPatch(typeof(PlayFabConnection), "OnResult")]
+    [HarmonyLib.HarmonyPatch(typeof(PlayFabConnection), "OnResult")]
     class OnResult
     {
         static bool Prefix(ref bool __runOriginal, LoginResult result)
@@ -43,6 +77,20 @@ namespace LoginCapture
             LoginCapture.Finalize(result);
             
             __runOriginal = false;
+            return false;
+        }
+    }
+    
+    // [HarmonyLib.HarmonyPatch(typeof(PlayFabConnection), "CheckEntitlement")]
+    class CheckEntitlement
+    {
+        static bool Prefix(ref bool __runOriginal,    
+            string loginResultText,
+            Il2CppSystem.Action<LoginResult> resultCallback,
+            Il2CppSystem.Action<PlayFabError> errorCallback,
+            GetPlayerCombinedInfoRequestParams infoParams)
+        {
+            MelonLogger.Msg(loginResultText);
             return false;
         }
     }
